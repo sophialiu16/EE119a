1bbdbbfacdfea12acc0f702a1fad9d8d8de8bc4a
sophialiu16 <sophialiu16@gmail.com>
2018-11-16 05:57:20 -0800

    updated comments

--- HW6/NTSC.vhd
+++ HW6/NTSC.vhd
@@ -1,93 +1,204 @@
---use IEEE.STD_LOGIC_1164.ALL;
---use IEEE.STD_LOGIC_ARITH.ALL;
---use IEEE.STD_LOGIC_UNSIGNED.ALL;
-
----- Uncomment the following library declaration if instantiating
----- any Xilinx primitives in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 ----------------------------------------------------------------------------
+-- 
+-- EE119 HW6 
+-- 
+-- NTSC Controller
 --
--- NTSC Controller EE119 HW6
+-- This is a NTSC controller designed to output a color image stored in a 
+-- 512Kx8 EPROM to an NTSC monitor. The SYNC and BLANK NTSC timing signals 
+-- are generated, using a 10 MHz controller clock, along with the 
+-- EPROM addresses. Each image line is 512 pixels, and the even and odd lines 
+-- are interlaced on the display. 
+-- The outputs are timed as followed. The first output timing lines are 
+-- blanked for vertical synchronization, with equalizing pulses, 
+-- serration pulses, additional equalizing pulses, and blanked video 
+-- lines. The even video lines are then displayed, followed by 
+-- blanked lines and the odd video lines. The EPROM addresses are 
+-- pipelined and latched with an external latch. The result is a 
+-- 512x512 8 bit image with 3 red, 3 green, and 2 blue bits.
 --
--- Desc.
+-- A moore state machine is used, with the states shown below. 
+-- There are states for each of the vertical synchronization 
+-- and blanking pulses, and for outputing horizontal video 
+-- data. 
+-- 
+-- States: 
+--
+--		SYNC equalizing pulse occuring in the first half of a line
+--	EQ_LOW_STATE,			SYNC is first held low for EQ_LOW clocks. 
+--	EQ_HIGH_STATE,			SYNC is then held high for the rest of 
+-- 							the half-line
+--
+--		SYNC equalizing pulse occuring in the second half of a line
+--	EQ_LOW_STATE2,			SYNC is first held low for EQ_LOW clocks
+--	EQ_HIGH_STATE2,		SYNC is then held high for the rest of 
+--								the half-line
+--	
+-- 	SYNC serration pulse occuring in the first half of a line 
+--	SER_LOW_STATE,			SYNC is held low for SER_LOW clocks
+--	SER_HIGH_STATE,		SYNC is then held high
+--
+-- 	SYNC serration pulse occuring in the second half of a line 
+--	SER_LOW_STATE2,		SYNC is held low for SER_LOW clocks
+--	SER_HIGH_STATE2,		SYNC is then held high
+--
+--		SYNC blank pulse 
+--	BLANK_LOW_STATE,		SYNC is held low for BLANK_LOW clocks
+--	BLANK_HIGH_STATE,		SYNC is held high for the remaining half-line
+--	BLANK_HIGH2_STATE,	SYNC is held high for the second half-line
+--	
+-- 	Normal horizontal even/odd video lines
+--	DATA_LOW_STATE,		SYNC is held low for DATA_LOW clocks
+--	DATA_BP_STATE,			Back porch, blanked for DATA_BP clocks
+--	DATA_ADDR_STATE,		Data is sent, with the column address incremented
+--	DATA_FP_STATE			Front porch, blanked for DATA_FP clocks
+-- 
+-- State machine diagram: 
 --
 -- Ports:
+--		clk 		: IN std_logic;
+-- 		1 bit, 10 MHz input clock signal 
+-- 	
+--		sync 		: out std_logic;
+-- 		1 bit, inverted (active high) sync timing signal 
+-- 
+--		blank 	: out std_logic;
+--    	1 bit, active low blank timing signal 
+-- 
+--		odd 		: out std_logic;
+-- 		1 bit EPROM address output indicating even field output when '0' 
+--			and odd field output when '1'
+-- 
+--		addrrow 	: out std_logic_vector(7 downto 0);
+--		 	8 bit EPROM address row output, with MSB 7. 
+-- 		Even lines arelocated at EPROM rows 0 to 239 
+-- 		(beginning at addrrow = "00000000", odd = '0'), 
+-- 		and odd video lines are located at EPROM rows 256 to 495 
+-- 		(beginning at addrrow = "00000000", odd = '1')
+-- 
+--		addrcol 	: out std_logic_vector(9 downto 0); 
+--			10 bit EPROM address column output, with MSB 9
 --
 -- Revision History:
--- 11/14/18 Sophia Liu Initial revision.
--- 11/15/18 Sophia Liu Updated comments.
+-- 11/14/18 Sophia Liu Initial revision
+-- 11/15/18 Sophia Liu Added more states 
+-- 11/16/18 Sophia Liu Updated comments
 --
 ----------------------------------------------------------------------------
-LIBRARY ieee;
-USE ieee.std_logic_1164.ALL;
-USE ieee.numeric_std.ALL;
-ENTITY NTSC IS
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+entity NTSC is
 	PORT (
-		clk : IN std_logic;
-		sync : OUT std_logic;
-		blank : OUT std_logic;
-		odd : OUT std_logic;
-		addrrow : OUT std_logic_vector(7 DOWNTO 0);
-		addrcol : OUT std_logic_vector(9 DOWNTO 0)); 
+		clk 		: in std_logic; 
+		-- 	1 bit, 10 MHz input clock signal
+		
+		sync 		: out std_logic;
+		-- 	1 bit, inverted (active high) sync timing signal 
+		
+		blank 	: out std_logic;
+		--   1 bit, active low blank timing signal 
+		
+		odd 		: out std_logic;
+		-- 	1 bit EPROM address output indicating even field output when '0' 
+		--			and odd field output when '1'
+		
+		addrrow 	: out std_logic_vector(7 downto 0);	
+		-- 8 bit EPROM address row output, with MSB 7. 
+		-- Even lines arelocated at EPROM rows 0 to 239 
+		-- (beginning at addrrow = "00000000", odd = '0'), 
+		-- and odd video lines are located at EPROM rows 256 to 495 
+		-- (beginning at addrrow = "00000000", odd = '1')
+		
+		addrcol 	: out std_logic_vector(9 downto 0)); 
+		--	10 bit EPROM address column output, with MSB 9
 END NTSC;
 
+
 ARCHITECTURE Behavioral OF NTSC IS
-	SIGNAL holdCount : INTEGER RANGE 1 TO 320 := 1;
-	TYPE states IS (
-	EQ_LOW_STATE,
+
+	-- Moore state machine for NTSC controller
+	TYPE NTSCstates IS (
+	-- Equalizing pulse
+	EQ_LOW_STATE,  
 	EQ_HIGH_STATE,
 	EQ_LOW_STATE2,
 	EQ_HIGH_STATE2,
 	
+	-- Serration pulse
 	SER_LOW_STATE,
 	SER_HIGH_STATE,
 	SER_LOW_STATE2,
 	SER_HIGH_STATE2,
 	
+	-- Blanking line 
 	BLANK_LOW_STATE,
 	BLANK_HIGH_STATE,
 	BLANK_HIGH2_STATE,
 	
+	-- Normal video line 
 	DATA_LOW_STATE,
 	DATA_BP_STATE,
 	DATA_ADDR_STATE,
-	--DATA_ADDR2_STATE,
 	DATA_FP_STATE
 	);
-	--attribute enum_encoding : string;
-	--attribute enum_encoding of states : type is "sequential";
-	SIGNAL State : states := EQ_LOW_STATE;
 	
-	constant EQ_LOW: integer := 23; 
-	constant SER_LOW : INTEGER  := 273;
-	constant BLANK_LOW : INTEGER:= 47;
-	constant DATA_LOW : INTEGER:= 47;
-	constant DATA_BP : INTEGER := 62;
-	constant DATA_FP : INTEGER := 15;
-	constant PULSEPER : INTEGER := 318;
+	-- current state  for NTSC state machine 
+	SIGNAL state : NTSCstates := EQ_LOW_STATE;
 	
-	signal row : std_logic_vector(7 DOWNTO 0);-- := "00000000";
-	signal col : std_logic_vector(9 DOWNTO 0);-- := "0000000000";
+	-- Constants 
 	
-
-BEGIN
-	PROCESS (clk)
-	variable dline : INTEGER RANGE 1 TO 526 := 1;
-	BEGIN
+	-- Timing constants
+	constant EQ_LOW	 : integer := 23; 
+	-- 	Number of clocks to hold SYNC low for equalizing pulse
+	constant SER_LOW	 : integer := 273;
+	-- 	Number of clocks to hold SYNC low for serration pulse
+	constant BLANK_LOW : integer := 47;
+	-- 	Number of clocks to hold SYNC low for blank line
+	constant DATA_LOW  :	integer := 47;
+	-- 	Number of clocks to hold SYNC low before addressing EPROM
+	constant DATA_BP   :	integer := 62;
+	-- 	Number of clocks to wait before sending video data
+	constant DATA_FP   :	integer := 15;
+	-- 	Number of clocks to wait after sending video data
+	constant PULSEPER  :	integer := 318;
+	-- 	Number of clocks for half a line ("pulse period")
+	
+	-- Line constants 
+	constant LINETOTAL : integer := 525; 
+	-- 	Total number of lines to output 
+	
+	
+	-- Internal signals 
+	-- 8 bit internal EPROM row address signal 
+	signal row : std_logic_vector(7 DOWNTO 0);
+	
+	-- 10 bit internal EPROM column address signal 
+	signal col : std_logic_vector(9 DOWNTO 0);
+	
+	-- internal integer for counting clocks up to half a line (PULSEPER clocks)
+	signal holdCount : integer range 1 to PULSEPER := 1;
+	
+	begin
+	process (clk)
+	-- internal counter that keeps track of current line number
+	variable dline : INTEGER RANGE 1 TO LINETOTAL + 1 := 1;
+	begin
 		IF rising_edge(clk) THEN
-			CASE State IS
+			CASE state IS
 				WHEN EQ_LOW_STATE =>
 					IF holdCount = EQ_LOW THEN
+						-- move to rest of equalizing pulse after EQ_LOW clocks
 						State <= EQ_HIGH_STATE;
 					ELSE
+						-- wait for correct number of clocks to elapse
 						State <= EQ_LOW_STATE;
 					END IF;
 
 				WHEN EQ_HIGH_STATE =>
 					IF holdCount = PULSEPER THEN
-							State <= EQ_LOW_STATE2; 
-						IF dline = 266 THEN 
+							--State <= EQ_LOW_STATE2; TODO remove
+						IF dline = 266 THEN --TODO magic numbers
 							State <= SER_LOW_STATE2;
 						ELSIF dline = 272 THEN 
 							State <= BLANK_HIGH2_STATE;
@@ -187,7 +298,6 @@ BEGIN
 						dline := dline + 1;
 						IF dline > 525 THEN
 							dline := 1;
-						--ELSE
 						END IF;
 					ELSE
 						State <= BLANK_HIGH2_STATE;
@@ -203,10 +313,10 @@ BEGIN
 				WHEN DATA_BP_STATE =>
 					IF holdCount = DATA_LOW + DATA_BP THEN
 						State <= DATA_ADDR_STATE;
-						IF dline = 21 then -->= 21 and dline <= 260 THEN
+						IF dline = 21 then 
 							odd <= '0';
 							row <= "00000000";
-						ELSIF dline = 284 then -- dline = 284
+						ELSIF dline = 284 then 
 							odd <= '1';
 							row <= "00000000";
 						END IF;
@@ -225,32 +335,18 @@ BEGIN
 						State <= DATA_ADDR_STATE;
 					END IF;
 
---				WHEN DATA_ADDR2_STATE =>
---					addrcol <= col;
---					col <= std_logic_vector(unsigned(col) + 1);
---					IF holdCount = PULSEPER - DATA_FP THEN
---						State <= DATA_FP_STATE;
---					ELSE
---						State <= DATA_ADDR2_STATE;
---					END IF;
-
 				WHEN DATA_FP_STATE =>
 					IF holdCount = PULSEPER THEN
 						IF dline = 260 OR dline = 523 THEN
 							State <= BLANK_LOW_STATE;
 						ELSE
 							row <= std_logic_vector(unsigned(row) + 1);
-							--col <= "0000000000";
-							--addrrow <= row;
 							State <= DATA_LOW_STATE;
 						END IF;
 						dline := dline + 1;
 					ELSE
 						State <= DATA_FP_STATE;
 					END IF;
-				
-				--when others => 
-					--State <= EQ_LOW_STATE;
 			END CASE;
 			
 			IF holdCount = PULSEPER THEN
@@ -301,7 +397,7 @@ BEGIN
 				WHEN DATA_LOW_STATE =>
 					sync <= '1';
 					blank <= '1';
-				WHEN DATA_BP_STATE => --DATA_ADDR2_STATE |
+				WHEN DATA_BP_STATE => 
 					sync <= '0';
 					blank <= '1';
 				WHEN DATA_ADDR_STATE => 
@@ -310,198 +406,7 @@ BEGIN
 				WHEN DATA_FP_STATE => 
 					sync <= '0'; 
 					blank <= '1'; 
-				--when others =>
-					--sync <= '0';
-					--blank <= '0';
 			END CASE;
 		END PROCESS;
 
 END Behavioral;
-
-
-
-----use IEEE.STD_LOGIC_1164.ALL;
-----use IEEE.STD_LOGIC_ARITH.ALL;
-----use IEEE.STD_LOGIC_UNSIGNED.ALL;
---
------- Uncomment the following library declaration if instantiating
------- any Xilinx primitives in this code.
-----library UNISIM;
-----use UNISIM.VComponents.all;
-------------------------------------------------------------------------------
-----
----- NTSC Controller EE119 HW6
-----
----- Desc.
-----
----- Ports:
-----
----- Revision History:
----- 11/14/18 Sophia Liu Initial revision.
----- 11/15/18 Sophia Liu Updated comments.
-----
-------------------------------------------------------------------------------
---LIBRARY ieee;
---USE ieee.std_logic_1164.ALL;
---USE ieee.numeric_std.ALL;
---ENTITY NTSC IS
---	PORT (
---		clk : IN std_logic;
---		sync : OUT std_logic;
---		blank : OUT std_logic;
---		odd : OUT std_logic;
---		addrrow : OUT std_logic_vector(7 DOWNTO 0); 
---		addrcol : OUT std_logic_vector(9 DOWNTO 0)); 
---END NTSC;
---
---ARCHITECTURE Behavioral OF NTSC IS
---	SIGNAL holdCount : INTEGER RANGE 0 TO 637 := 0;
---	SIGNAL dline : INTEGER RANGE 1 TO 525 := 1;
---	--SIGNAL blank : std_logic;
---	signal row : std_logic_vector(7 DOWNTO 0); --:= "00000000";
---	signal col : std_logic_vector(9 DOWNTO 0); --:= "0000000000";
---	--signal sync: std_logic;
---	
---	constant EQ_LOW: natural := 23; 
---	constant SER_LOW : natural := 273;
---	constant BLANK_LOW : natural := 47;
---	constant DATA_LOW : natural := 47;
---	constant DATA_BP : natural := 62;
---	constant DATA_FP : natural := 15;
---	constant PULSEPER : natural := 318;
---	constant LINEPER : natural := 636;
---BEGIN
---	PROCESS (clk)
---	BEGIN
---		IF rising_edge(clk) THEN
---		case dline is 
---			when 1 to 3 | 7 to 9 | 264 | 265 | 270 | 271 =>
---				-- 2 eq pulses 
---					--blank <= '0';
---				if holdCount = PULSEPER then 
---				--(holdCount <= EQ_LOW) or (holdCount > PULSEPER and holdCount <= EQ_lOW + PULSEPER) then 
---					sync <= '0';
---				elsif holdCount = EQ_LOW or holdCount = EQ_LOW + PULSEPER then 
---				--(holdCount > EQ_LOW and holdCount <= PULSEPER) or (holdCount > EQ_LOW + PULSEPER and holdCount <= LINEPER)
---					sync <= '1'; 
---				end if; 
---				
---			when 4 to 6 | 267 | 268 =>
---				-- 2 ser pulses
---				--blank <= '0';
---				if holdCount = PULSEPER then 
---				--(holdCount <= SER_LOW) or (holdCount > PULSEPER and holdCount <= SER_lOW + PULSEPER) then 
---					sync <= '0';
---				elsif holdCount = SER_LOW or holdCount = SER_LOW + PULSEPER then 
---				--(holdCount > SER_LOW and holdCount <= PULSEPER) or 
---				--(holdCount > EQ_LOW + PULSEPER and holdCount <= LINEPER) 
---					sync <= '1'; 
---				end if; 
---
---			when 10 to 20 | 273 to 283 | 523 to 525 | 261 to 262 =>
---				-- blank pulse
---				blank <= '0';
---				--if holdCount = 0 then --(holdCount <= BLANK_LOW) then 
---				--	sync <= '0';
---				if holdCount = BLANK_LOW then 
---					sync <= '1'; 
---				end if; 
---					
---			when 263 => 
---				-- blank/eq 
---				--blank <= '0';
---				--if holdCount = 0 then --(holdCount <= BLANK_LOW) then 
---					--sync <= '0';
---				if holdCount = BLANK_LOW then --(holdCount  <= PULSEPER) then 
---					sync <= '1'; 
---				elsif holdCount = PULSEPER then --(holdCount <= PULSEPER + EQ_LOW) then 
---					sync <= '0'; 
---				elsif holdCount = PULSEPER + EQ_LOW then 
---					sync <= '1';
---				end if; 
---				
---			when 266 => 
---				-- eq/ser  
---				--blank <= '0';
---				--if holdCount = 0 then --(holdCount <= EQ_LOW) then 
---					--sync <= '0';
---				if holdCount = EQ_LOW then -- (holdCount  <= PULSEPER) then 
---					sync <= '1'; 
---				elsif holdCount = PULSEPER then --(holdCount <= PULSEPER + SER_LOW) then 
---					sync <= '0'; 
---				elsif holdCount = PULSEPER + SER_LOW then 
---					sync <= '1';
---				end if; 
---				
---			when 269 =>
---				-- ser/eq 
---				--blank <= '0';
---				--if holdCount = 0 then --(holdCount <= SER_LOW) then 
---					--sync <= '0';
---				if holdCount = SER_LOW then --(holdCount  <= PULSEPER) then 
---					sync <= '1'; 
---				elsif holdCount = PULSEPER then --(holdCount <= PULSEPER + EQ_LOW) then 
---					sync <= '0'; 
---				elsif holdCount = PULSEPER + EQ_LOW then
---					sync <= '1';
---				end if; 
---				
---			when 272 =>
---				-- eq/blank
---				--blank <= '0';
---				--if holdCount = 0 then --(holdCount <= EQ_LOW) then 
---				--	sync <= '0';
---				if holdCount = EQ_LOW then 
---					sync <= '1';
---				end if; 
---				
---			when 21 to 260 | 284 to 522 =>
---				-- even 
---				blank <= '1';
---				if dline = 21 then 
---					row <= "00000000"; 
---					odd <= '0'; 
---				elsif dline = 284 then 
---					row <= "00000000";
---					odd <= '1'; 
---				end if ; 
---				
---				--if holdCount = 0 then --(holdCount <= DATA_LOW) then -- hold low 
---					--sync <= '0';
---				if holdCount = DATA_LOW then --(holdCount <= DATA_LOW + DATA_BP) then -- hold back porch 
---					sync <= '1';
---					col <= "0000000000"; 
---				elsif holdCount = DATA_LOW + DATA_BP then --(holdCount <= DATA_LOW + DATA_BP + 512) then 
---					sync <= '1'; 
---					addrrow <= row; 
---					addrcol <= col; 
---					col <= std_logic_vector(unsigned(col) + 1); 
---				elsif holdCount = DATA_LOW + DATA_BP + 512 then 
---					sync <= '0'; 
---				end if; 
---				
---				if holdCount >= LINEPER then 
---					--dline := dline + 1; 
---					row <= std_logic_vector(unsigned(row) + 1); 
---				end if; 
---			end case;
---			
---			IF holdCount = LINEPER THEN
---				holdCount <= 0;
---				sync <= '0'; --TODO offby1
---				if dline = 525 then 
---					dline <= 1; 
---				else 
---					dline <= dline + 1; 
---				end if; 	
---			ELSE
---				holdCount <= holdCount + 1;
---			END IF;
---			
---			--syncout <= sync; 
---			--blankout <= blank; 
---			
---		end if; 
---	END PROCESS;
---	
---END Behavioral;
